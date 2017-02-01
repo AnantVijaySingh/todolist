8cbd2a385d6a877a43d05618f814c0ca6fae7e30
Anant <anantvijay.23@gmail.com>
2017-02-01 22:13:48 +0000

    Layout

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+// window.onload = init;
+
+// function init(){
+//  var button = document.getElementById("addButton");
+//  button.onclick = handleButtonClick;
+// }
+
+// function handleButtonClick() {
+//  var textInput = document.getElementById("songTextInput");
+//  var songName = textInput.value
+//  var list = document.getElementById("playlist");
+//  if (songName.length == "") {
+//    alert("Error");
+//  } else {
+//    var listElement = document.createElement("li");
+//    listElement.innerHTML = songName;
+//    list.appendChild(listElement);
+//  }
+// }
