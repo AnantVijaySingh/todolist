ec0520e19a136b8ed130e1199e98b5d4cb92a945
Anant <anantvijay.23@gmail.com>
2017-02-10 22:19:38 +0000

    task obeject constructor and task DOM card

--- todo.js
+++ todo.js
@@ -18,3 +18,22 @@ function handleTodayButtonClick(){
 	var userTask = taskTextField.value;
 	alert(userTask);
 }
+// ---------- Task Object Constructor ----------
+// ----- add functioanlity for time -----
+function TaskObject(taskText,taskType){
+	this.taskText = taskText;
+	this.taskType = taskType;
+	this.taskCompleteStatus = "incomplete";		//Possible statues: incomplete, complete
+	this.taskProgressStatus = "notStarted";		//Possible statues: notStarted, inProgress, paused
+	this.timeStamp = new Date().getTime();		// Compare with current time to determine is the task counts under today or backlog.
+												//In conjuction with week paramter, add to this week initially and to backlog only if its the next week
+	
+	var listElement = document.createElement("li");
+	listElement.innerHTML = songName;
+	list.appendChild(listElement);
+}
+
+// ---------- DOM Element Creator Fucntion ----------
+function taskCard(TaskObject){
+	var parentElement = document.getElementById("taskList");
+}
